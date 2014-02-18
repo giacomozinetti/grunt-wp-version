58811d2dd45b93b49f64bc7ea486a2216a28ac0f
Giacomo Zinetti <giacomo.zinetti@bryan.it>
2014-02-18 18:12:02 +0100

    assets filename with hash

--- tasks/version.js
+++ tasks/version.js
@@ -16,21 +16,43 @@ module.exports = function(grunt) {
 
     var scriptsPhp = options.file;
 
-    // Hash the CSS
+    // Hash
     var hashCss = md5(options.css);
-
-    // Hash the JS
     var hashJs = md5(options.js);
 
-    // Update scripts.php to reference the new versions
-    var regexCss = new RegExp("(wp_enqueue_style\\('" + options.cssHandle + "',(\\s*[^,]+,){2})\\s*[^\\)]+\\);");
-    var regexJs = new RegExp("(wp_register_script\\('" + options.jsHandle + "',(\\s*[^,]+,){2})\\s*[^,]+,\\s*([^\\)]+)\\);");
+    if (!grunt.file.exists(scriptsPhp)) {
+      var content = "<?php\n";
+      grunt.file.write(scriptsPhp, content);
+    }
+
+    // Update versions.php to reference the new versions
+    var regexCss = new RegExp("define\\('" + options.cssHandle + "', '(\\w*)'\\);");
+    var regexJs = new RegExp("define\\('" + options.jsHandle + "', '(\\w*)'\\);");
 
     var content = grunt.file.read(scriptsPhp);
-    content = content.replace(regexCss, "$1 '" + hashCss + "');");
-    content = content.replace(regexJs, "$1 '" + hashJs + "', " + "$3);");
+    var cssMatches = content.match(regexCss);
+    if (cssMatches === null) {
+      content += "\tdefine('" + options.cssHandle + "', '" + hashCss + "');\n";
+    } else {
+      if (grunt.file.exists(options.css.replace('.css', '.' + cssMatches[1] + '.css'))) {
+        grunt.file.delete(options.css.replace('.css', '.' + cssMatches[1] + '.css'));
+      }
+      grunt.file.copy(options.css, options.css.replace('.css', '.' + hashCss + '.css'));
+      content = content.replace(regexCss, "define('" + options.cssHandle + "', '" + hashCss + "');");
+    }
+    var jsMatches = content.match(regexJs);
+    if (jsMatches === null) {
+      content += "\tdefine('" + options.jsHandle + "', '" + hashJs + "');\n";
+    } else {
+      if (grunt.file.exists(options.js.replace('.js', '.' + jsMatches[1] + '.js'))) {
+        grunt.file.delete(options.js.replace('.js', '.' + jsMatches[1] + '.js'));
+      }
+      grunt.file.copy(options.js, options.js.replace('.js', '.' + hashJs + '.js'));
+      content = content.replace(regexJs, "define('" + options.jsHandle + "', '" + hashJs + "');");
+    }
     grunt.file.write(scriptsPhp, content);
     grunt.log.writeln('"' + scriptsPhp + '" updated with new CSS/JS versions.');
+
   });
 
   var md5 = function(filepath) {
